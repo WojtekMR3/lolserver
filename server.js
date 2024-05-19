e4291c8497078a3dee80fbff134d8eabf7651b95
wojtekrfx <r3flexmlg@gmail.com>
2024-05-19 13:19:30 +0000

    1

--- server.js
+++ server.js
@@ -301,10 +301,31 @@ function regionToContinent(region) {
 }
 
 // Enable CORS middleware
+// const corsOptions = {
+//     //origin: 'http://localhost:54545'
+//     origin: 'https://lolvue.vercel.app'
+// };
+
 const corsOptions = {
-    //origin: 'http://localhost:54545'
-    origin: 'https://lolvue.vercel.app'
-};
+    origin: (origin, callback) => {
+      // List of allowed origins
+      const allowedOrigins = ['http://localhost:54545', 'https://lolvue.vercel.app', 'http://lolvue.vercel.app'];
+  
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+  
+      // Check if the origin is in the list of allowed origins
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 204
+  };
 
 app.use(cors(corsOptions));
 
